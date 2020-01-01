ea016d388c3be2b01f531d89a1b1aea3c9a9a518
Pauline Kong <pkong89@gmail.com>
2020-01-01 11:25:52 -0500

    callback chaining

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,15 +1,25 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('Florida', (error, data) => {
-  console.log('Error', error);
-  console.log('Data', data);
-});
+const address = process.argv[2];
 
-forecast(-75.7088, 44.1545, (error, data) => {
-  console.log('Error', error);
-  console.log('Data', data);
-});
+if (!address) {
+  console.log('Please provide an address');
+} else {
+  //callback chaining
+  geocode(address, (error, data) => {
+    if (error) {
+      return console.log(error);
+    }
+    forecast(data.longitude, data.latitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log(data.location);
+      console.log(forecastData);
+    });
+  });
+}
 
 // previous code prior to forecast.js
 // const url =
