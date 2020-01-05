42babba6e1666f9962941a7d5cacacb2058ee8aa
Pauline Kong <pkong89@gmail.com>
2020-01-05 12:48:53 -0500

    built endpoint for weather

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,6 +3,9 @@ const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
 
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
 const app = express();
 
 //Define paths for Express config
@@ -38,11 +41,51 @@ app.get('/help', (req, res) => {
 
 //404 catch page for help
 app.get('/help/*', (req, res) => {
-  res.send('Help article not found');
+  res.render('404', {
+    title: '404',
+    name: 'Andrew Mead',
+    errorMessage: 'Help article not found',
+  });
 });
 
 app.get('/weather', (req, res) => {
-  res.send({ forecast: 'sunny' }, { location: 'New York' });
+  if (!req.query.address) {
+    return res.send({
+      error: 'address must be provided',
+    });
+  }
+  geocode(req.query.address, (error, { longitude, latitude, location }) => {
+    if (error) {
+      return res.send({ error });
+    }
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({ error });
+      }
+      res.send({
+        forecast: forecastData,
+        location,
+        address: req.query.address,
+      });
+    });
+  });
+  // res.send({
+  //   forecast: 'sunny',
+  //   location: 'texas',
+  //   address: req.query.address,
+  // });
+});
+
+app.get('/products', (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: 'you must provide a search term',
+    });
+  }
+  console.log(req.query.search);
+  res.send({
+    products: [],
+  });
 });
 
 //* = wildcard character - will match when everything else above does not
---
+++ web-server/src/utils/forecast.js
@@ -0,0 +1,26 @@
+const request = require('request');
+
+const forecast = (longitude, latitude, callback) => {
+  const url =
+    'https://api.darksky.net/forecast/a703be8465183e9f437c71e70b67e38d/' +
+    latitude +
+    ',' +
+    longitude;
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect to weather services!', undefined);
+    } else if (body.error) {
+      callback('Unable to find location. Please try another search', undefined);
+    } else {
+      callback(
+        undefined,
+
+        body.daily.data[0].summary +
+          ` It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`
+      );
+    }
+  });
+};
+
+module.exports = forecast;
