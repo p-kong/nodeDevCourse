7b9e3d22079a52459feb9d6337c79b563214c706
Pauline Kong <pkong89@gmail.com>
2020-01-05 15:44:05 -0500

    completed weather app

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -7,13 +7,29 @@ console.log('Client side javascript file is loaded');
 //   });
 // });
 
-fetch('http://localhost:3000/weather?address=boston').then(response => {
-  response.json().then(data => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data.location);
-      console.log(data.forecast);
-    }
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+// messageOne.textContent = 'From JavaScript';
+// messageTwo.textContent = 'Also From JavaScript';
+
+weatherForm.addEventListener('submit', e => {
+  e.preventDefault();
+  let location = search.value;
+
+  messageOne.textContent = 'Loading...';
+  messageTwo.textContent = '';
+
+  fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
+    response.json().then(data => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
   });
 });
